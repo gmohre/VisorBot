3d53ee3f89932ad3427219356e5704fa977b1dc1
glenn <glenn.mohre@gmail.com>
2018-02-10 16:32:11 -0500

    updated populate data

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import json
-import urllib2
+import urllib
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////C:\ow.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/gmohre/ow.db'
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
 OW_API_HEADERS = {"Accept": "application/json", "User-Agent": "Mozilla/5.0"}
-QUOTES = {}
+QUOTES = {}                                                                                   
 
 class Hero(db.Model):
     __tablename__ = 'hero'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(pow(2, 7)), nullable=False)
-    quotes = orm.relationship("Quote", backref="hero")
+    quotes = db.relationship("Quote", backref="hero")
 
     def __repr__(self):
         return '<Hero %r>' % self.name
@@ -31,12 +31,16 @@ class Quote(db.Model):
     def __repr__(self):
         return '<Quote %r - %r>' % (self.hero, self.text)
 
-def hero():
-    h=Hero(name='Ana')
-    q=Quote(text='Hi', hero=h)
-    db.session.add(h)
-    db.session.add(q)
-    db.session.commit()
+class HeroTest():
+    def setup(self):
+        h=Hero(name='Ana')
+        q=Quote(text='Hi', hero=h)
+        db.session.add(h)
+        db.session.add(q)
+        db.session.commit()
+
+    def test_quote(self):
+        assert self.quotes.all()
 
 
 
@@ -54,8 +58,13 @@ def populate_data():
         req = urllib2.Request(url=url, headers=OW_API_HEADERS)
         page = urllib2.urlopen(req)
         data = json.loads(page.read())
-        quotes = [quote['name'] for quote in data['rewards'] if quote['type']['name'] == 'voice line']
+        quotes = [{text=quote['name'], hero=hero} for quote in
+            data['rewards'] if quote['type']['name'] == 'voice line']
         return quotes
+
+    def _create_obj(model, data):
+        db.session.add(model(**data))
+        db.session.commit()
 
     try:
         QUOTES = json.load(open('data.txt'))
@@ -65,10 +74,9 @@ def populate_data():
         heroes = _load_heroes()
         print('getting quotes')
         for hero in heroes:
-            QUOTES["hero"+str(hero['id'])]= _load_quotes(hero)
-        with open("data.txt", 'w') as outfile:
-            json.dump(QUOTES, outfile)
-    return render_template('hero.html', **QUOTES)
+            _create_obj(Hero, hero)
+            [_create_obj(Quote, quote) for quote in _load_quotes(hero)]
+    return render_template('hero.html', Hero.query.all())
 
 @socketio.on('value changed')
 def value_changed(message):
